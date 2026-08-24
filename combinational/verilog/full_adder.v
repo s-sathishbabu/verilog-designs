0067d12a9878a9114507fb6833fdd95842424c6c
s-sathishbabu <sathishsrinivasan9597@gmail.com>
2026-08-24 11:30:25 +0530

    feat: Add full adder and Testbench

--- combinational/verilog/full_adder.v
+++ combinational/verilog/full_adder.v
@@ -1,15 +1,11 @@
 `timescale 1ns / 1ps
 
-// Gate level modelling 
-module full_adder(input a,b,cin,output sum,carry);
 
-wire w1,w2,w3;
-
-xor x1(sum,a,b,cin);
-
-and a1(w1,a,b);
-and a2(w2,a,cin);
-and a3(w3,b,cin);
-
-or o1(carry,w1,w2,w3);c:\Users\sathi\Music\vivado\project_3\project_3.srcs\sim_1\new\full_adder_tb.v
+module full_adder(input a_fa,b_fa,cin_fa, output reg sum_fa,carry_fa);
+     always@(*)
+       begin
+         sum_fa = a_fa ^ b_fa ^ cin_fa;
+         carry_fa = (a_fa & b_fa) | (b_fa & cin_fa) | (cin_fa & a_fa);
+       end
+         
 endmodule
